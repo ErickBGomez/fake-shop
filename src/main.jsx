36242248e6bce1ee532d288a1a160c28fc115a4b
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-02 12:54:33 -0600

    Configure React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 // * Temporal fix: TypeError Uncaught, cannot read properties of undefined (reading '_config')
 import { system } from "@chakra-ui/react/preset";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider value={system}>
-      <App />
+      <RouterProvider router={routes} />
     </ChakraProvider>
   </StrictMode>
 );

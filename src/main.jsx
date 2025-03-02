616ebe7bf4af708f44fc9aa0817c85a6bc826305
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-02 11:26:24 -0600

    Configure Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+// * Temporal fix: TypeError Uncaught, cannot read properties of undefined (reading '_config')
+import { system } from "@chakra-ui/react/preset";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );

2988b85c1f7e5bffa88d1c11191a7643b95bf62e
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-03 08:58:44 -0600

    Install lucide icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
+import { UserRound, ShoppingCart, Search } from "lucide-react";
 import styles from "./Header.module.scss";
+
+const navElements = ["Categories", "Sales", "Stores", "Contact"];
 
 const Header = () => {
   return (
     <header className={styles.mainHeader}>
-      <div className="content">Header</div>
+      <div className={styles.content}>
+        <div className={styles.navigation}>
+          <div className={styles.brand}>BRAND</div>
+          <nav className={styles.headerNav}>
+            <ul className={styles.navElements}>
+              {navElements.map((element, index) => (
+                <li key={index} className={styles.navElement}>
+                  {element}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.actions}>
+          <div className={styles.search}>
+            <Search />
+          </div>
+          <div className={styles.user}>
+            <UserRound />
+          </div>
+          <div className={styles.cart}>
+            <ShoppingCart />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };

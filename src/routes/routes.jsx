bfb7acb8d4c456f7d14cc210971caffeeaf4d3dd
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-04 10:06:00 -0600

    Fix: Cannot find Home component

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import Layout from "../pages/Layout/Layout";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import Search from "../pages/Search/Search";
 import ProductPage from "../pages/ProductPage/ProductPage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";

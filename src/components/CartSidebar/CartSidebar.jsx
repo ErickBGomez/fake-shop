9b82a2795b95afe8517582285329609c9f17e819
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-06 09:45:52 -0600

    Add sections to sidebar cart component

--- src/components/CartSidebar/CartSidebar.jsx
+++ src/components/CartSidebar/CartSidebar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import CartContext from "@/context/CartContext";
 import styles from "./CartSidebar.module.scss";
+import { Link } from "react-router-dom";
 
 const CartSidebar = () => {
   const { open } = useContext(CartContext);
@@ -9,8 +10,14 @@ const CartSidebar = () => {
     <>
       {open && (
         <div className={styles.sidebarCart}>
-          CartSidebar
-          {console.log("cart mounted")}
+          <div className={styles.details}>
+            <p>Subtotal</p>
+            {/* TODO: Calculate subtotal here */}
+            <p>$9.99</p>
+            <Link to="/cart">View Cart</Link>
+            <div className={styles.products}></div>
+          </div>
+          <hr />
         </div>
       )}
     </>

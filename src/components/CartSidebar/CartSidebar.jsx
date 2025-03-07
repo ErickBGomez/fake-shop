1b53ef680dc870ed84f7a89433d354e2f70866e8
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-06 22:32:46 -0600

    Create backdrop for sidebar cart component

--- src/components/CartSidebar/CartSidebar.jsx
+++ src/components/CartSidebar/CartSidebar.jsx
@@ -4,21 +4,24 @@ import styles from "./CartSidebar.module.scss";
 import { Link } from "react-router-dom";
 
 const CartSidebar = () => {
-  const { open } = useContext(CartContext);
+  const { open, setOpen } = useContext(CartContext);
 
   return (
     <>
       {open && (
-        <div className={styles.sidebarCart}>
-          <div className={styles.details}>
-            <p>Subtotal</p>
-            {/* TODO: Calculate subtotal here */}
-            <p>$9.99</p>
-            <Link to="/cart">View Cart</Link>
+        <>
+          <div className={styles.backdrop} onClick={() => setOpen(false)}></div>
+          <div className={styles.sidebarCart}>
+            <div className={styles.details}>
+              <p>Subtotal</p>
+              {/* TODO: Calculate subtotal here */}
+              <p>$9.99</p>
+              <Link to="/cart">View Cart</Link>
+            </div>
+            <hr />
             <div className={styles.products}></div>
           </div>
-          <hr />
-        </div>
+        </>
       )}
     </>
   );

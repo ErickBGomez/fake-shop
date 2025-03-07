e658baa90603176fd285f0d045e424f864c7c1c4
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 13:23:06 -0600

    Add tests for addProduct function

--- test/cart.test.js
+++ test/cart.test.js
@@ -1,1 +1,36 @@
-describe("Helper functions", () => {});
+import { addProduct } from "../src/helper/cart";
+
+describe("Helper functions", () => {
+  describe("addProduct", () => {
+    it("should add a product to the cart", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const newProduct = { id: 3, title: "Product 3", quantity: 3 };
+
+      const result = addProduct(allProducts, newProduct);
+
+      expect(result).toEqual([
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+        { id: 3, title: "Product 3", quantity: 3 },
+      ]);
+    });
+
+    it("should update the quantity of the existing product", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const newProduct = { id: 2, title: "Product 2", quantity: 3 };
+
+      const result = addProduct(allProducts, newProduct);
+
+      expect(result).toEqual([
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 5 },
+      ]);
+    });
+  });
+});

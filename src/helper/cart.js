74ebbcfb028f7a0e355673fed69c16b051b1af7f
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 13:44:48 -0600

    Bug fix: addProduct was returning less products instead of modifying the existing one

--- src/helper/cart.js
+++ src/helper/cart.js
@@ -1,16 +1,22 @@
-export const addProduct = (allProducts = [], newProduct = {}) => {
-  const index = allProducts.indexOf((product) => product.id === newProduct.id);
+export const addProduct = (products = [], newProduct = {}) => {
+  const existingProduct = products.find(
+    (product) => product.id === newProduct.id
+  );
 
   // Add new product at the end of array if it does not exists in allProducts
-  if (index === -1) return [...allProducts, newProduct];
+  if (!existingProduct) return [...products, newProduct];
 
-  const existingProduct = allProducts[index];
+  const index = products.indexOf(existingProduct);
 
   // Add the previous quantity with the new one if the product already exists
-  const b = {
+  const updatedProduct = {
     ...existingProduct,
     quantity: existingProduct.quantity + newProduct.quantity,
   };
 
-  return allProducts.splice(index, 1, b);
+  const updatedProducts = products.map((product) =>
+    product.id === newProduct.id ? updatedProduct : product
+  );
+
+  return updatedProducts;
 };

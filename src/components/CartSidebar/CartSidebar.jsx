f66fc4bc1b9cc64281864a12d860595b959e3e89
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-06 22:35:14 -0600

    Add icon to close sidebar

--- src/components/CartSidebar/CartSidebar.jsx
+++ src/components/CartSidebar/CartSidebar.jsx
@@ -1,18 +1,24 @@
 import { useContext } from "react";
-import CartContext from "@/context/CartContext";
-import styles from "./CartSidebar.module.scss";
 import { Link } from "react-router-dom";
+import { X } from "lucide-react";
+import CartContext from "../../context/CartContext";
+import styles from "./CartSidebar.module.scss";
 
 const CartSidebar = () => {
   const { open, setOpen } = useContext(CartContext);
+
+  const handleCloseSidebar = () => {
+    setOpen(false);
+  };
 
   return (
     <>
       {open && (
         <>
-          <div className={styles.backdrop} onClick={() => setOpen(false)}></div>
+          <div className={styles.backdrop} onClick={handleCloseSidebar}></div>
           <div className={styles.sidebarCart}>
             <div className={styles.details}>
+              <X onClick={handleCloseSidebar} />
               <p>Subtotal</p>
               {/* TODO: Calculate subtotal here */}
               <p>$9.99</p>

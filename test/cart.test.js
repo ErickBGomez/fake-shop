4d625208fc1b4e540e91f48acaf9c6379c418e59
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 13:55:11 -0600

    Create updateProducts and removeProducts tests

--- test/cart.test.js
+++ test/cart.test.js
@@ -1,4 +1,4 @@
-import { addProduct } from "../src/helper/cart";
+import { addProduct, updateProduct, removeProduct } from "../src/helper/cart";
 
 describe("Helper functions", () => {
   describe("addProduct", () => {
@@ -30,6 +30,67 @@ describe("Helper functions", () => {
       expect(result).toEqual([
         { id: 1, title: "Product 1", quantity: 1 },
         { id: 2, title: "Product 2", quantity: 5 },
+      ]);
+    });
+  });
+
+  describe("updateProduct", () => {
+    it("should update the quantity of the existing product", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const updatedProduct = { id: 2, title: "Product 2", quantity: 3 };
+
+      const result = updateProduct(allProducts, updatedProduct);
+
+      expect(result).toEqual([
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 3 },
+      ]);
+    });
+
+    it("should return the same products if the product does not exist", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const updatedProduct = { id: 3, title: "Product 3", quantity: 3 };
+
+      const result = updateProduct(allProducts, updatedProduct);
+
+      expect(result).toEqual([
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ]);
+    });
+  });
+
+  describe("removeProduct", () => {
+    it("should remove the product from the cart", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const productId = 2;
+
+      const result = removeProduct(allProducts, productId);
+
+      expect(result).toEqual([{ id: 1, title: "Product 1", quantity: 1 }]);
+    });
+
+    it("should return the same products if the product does not exist", () => {
+      const allProducts = [
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
+      ];
+      const productId = 3;
+
+      const result = removeProduct(allProducts, productId);
+
+      expect(result).toEqual([
+        { id: 1, title: "Product 1", quantity: 1 },
+        { id: 2, title: "Product 2", quantity: 2 },
       ]);
     });
   });

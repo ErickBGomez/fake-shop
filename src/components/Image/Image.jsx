dfb9b68b90c954a4b1c79ad2ee17720d4ac61c0d
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 17:38:48 -0600

    Change image in preview

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -2,11 +2,11 @@ import { Image as ChakraImage } from "@chakra-ui/react";
 import { ImageOff } from "lucide-react";
 import styles from "./Image.module.scss";
 
-const Image = ({ src, alt, rounded = "md" }) => {
+const Image = ({ src, alt, rounded = "md", onClick }) => {
   return (
     <>
       {src ? (
-        <ChakraImage src={src} alt={alt} rounded={rounded} />
+        <ChakraImage src={src} alt={alt} rounded={rounded} onClick={onClick} />
       ) : (
         <div className={styles.fallbackImage}>
           <ImageOff />

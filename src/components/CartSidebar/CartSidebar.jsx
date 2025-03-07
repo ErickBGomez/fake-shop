6bced478fa298248df875dea34290480c23f5486
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 09:42:14 -0600

    Style empty cart

--- src/components/CartSidebar/CartSidebar.jsx
+++ src/components/CartSidebar/CartSidebar.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { X } from "lucide-react";
+import { X, ShoppingBag } from "lucide-react";
 import CartContext from "../../context/CartContext";
 import CartSidebarProduct from "../CartSidebarProduct/CartSidebarProduct";
 import styles from "./CartSidebar.module.scss";
@@ -19,23 +19,40 @@ const CartSidebar = () => {
         <>
           <div className={styles.backdrop} onClick={handleCloseSidebar}></div>
           <div className={styles.sidebarCart}>
-            <div className={styles.details}>
-              <div className={styles.close}>
-                <X onClick={handleCloseSidebar} />
+            <div className={styles.close}>
+              <X onClick={handleCloseSidebar} />
+            </div>
+            {state.products ? (
+              <>
+                <div className={styles.details}>
+                  <p className={styles.title}>Subtotal</p>
+                  {/* TODO: Calculate subtotal here */}
+                  <p className={styles.total}>$9.99</p>
+                  <Link to="/cart">
+                    <Button colorPalette="brand">View Cart</Button>
+                  </Link>
+                </div>
+                <hr />
+                <div className={styles.products}>
+                  {state?.products.map((product) => (
+                    <CartSidebarProduct key={product.id} product={product} />
+                  ))}
+                </div>
+              </>
+            ) : (
+              <div className={styles.empty}>
+                <ShoppingBag />
+                <p>Your cart is empty</p>
+                <p className={styles.subtitle}>
+                  Keep shopping and find amazing deals!
+                </p>
+                <Link to="/search">
+                  <Button colorPalette="brand" onClick={handleCloseSidebar}>
+                    Search products
+                  </Button>
+                </Link>
               </div>
-              <p className={styles.title}>Subtotal</p>
-              {/* TODO: Calculate subtotal here */}
-              <p className={styles.total}>$9.99</p>
-              <Link to="/cart">
-                <Button colorPalette="brand">View Cart</Button>
-              </Link>
-            </div>
-            <hr />
-            <div className={styles.products}>
-              {state?.products.map((product) => (
-                <CartSidebarProduct key={product.id} product={product} />
-              ))}
-            </div>
+            )}
           </div>
         </>
       )}

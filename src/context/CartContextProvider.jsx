984e976f0eaadb70370cce33672a0e3a1ebdd1fa
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-03-07 19:15:09 -0600

    Save and load products from local storage

--- src/context/CartContextProvider.jsx
+++ src/context/CartContextProvider.jsx
@@ -1,12 +1,26 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import CartContext from "./CartContext";
 import cartReducer from "../reducer/cartReducer";
+
+// Save and load products from local storage
+const saveProducts = (products) => {
+  localStorage.setItem("cartProducts", JSON.stringify(products));
+};
+
+const loadProducts = () => {
+  const products = localStorage.getItem("cartProducts");
+  return products ? JSON.parse(products) : [];
+};
 
 const CartContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, {
     isOpen: false,
-    products: [],
+    products: loadProducts(),
   });
+
+  useEffect(() => {
+    saveProducts(state.products);
+  }, [state.products]);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>

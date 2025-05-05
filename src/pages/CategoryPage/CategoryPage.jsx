1f0884df36543af245b60cd3c1934752e1cdbdc8
ErickBGomez <erickbgomez.dev@gmail.com>
2025-05-04 21:47:13 -0600

    Create category page and improve result container styles from search page

---
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -0,0 +1,90 @@
+import ResultContainer from "../../components/ResultContainer/ResultContainer";
+import ErrorPage from "../ErrorPage/ErrorPage";
+import styles from "./CategoryPage.module.scss";
+
+const CategoryPage = ({ category = "Category title" }) => {
+  // const result = [];
+  const result = [
+    {
+      id: 0,
+      image: null,
+      title: "Article 1",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 1,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 2,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 3,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 4,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 5,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 6,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+    {
+      id: 7,
+      image: null,
+      title: "Article 2",
+      rating: 4,
+      price: 9.99,
+      discount: 99,
+    },
+  ];
+
+  return (
+    <div className={styles.categoryPage}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>{category}</h1>
+      </div>
+      <div className={styles.content}>
+        {result.length > 0 ? (
+          <ResultContainer result={result} />
+        ) : (
+          <ErrorPage variant="page-not-found" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;

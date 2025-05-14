360aa3dfb127cbb068379394c1b653f2125c730e
ErickBGomez <erickbgomez.dev@gmail.com>
2025-05-14 06:04:20 -0600

    Load new results container in search page

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -20,88 +20,17 @@ const SearchPage = () => {
     return;
   }
 
-  // const result = [];
-  const results = [
-    {
-      id: 0,
-      image: null,
-      title: "Article 1",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 1,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 2,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 3,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 4,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 5,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 6,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-    {
-      id: 7,
-      image: null,
-      title: "Article 2",
-      rating: 4,
-      price: 9.99,
-      discount: 99,
-    },
-  ];
-
   return (
     <div className={styles.searchContainer}>
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Search results</h1>
-        <p>
-          {results.length} articles found for "{query}".
-        </p>
+        <p>Showing articles found for "{query}".</p>
       </div>
       <div className={styles.content}>
-        {results.length > 0 ? (
-          <ResultsContainer results={results} />
-        ) : (
-          <ErrorPage variant="search-no-results" />
-        )}
+        <ResultsContainer
+          fetchPath={`products/search`}
+          queries={`q=${query}`}
+        />
       </div>
     </div>
   );

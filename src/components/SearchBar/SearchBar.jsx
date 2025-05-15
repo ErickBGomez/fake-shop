d076b34f49b5ac40f98e4c522bc7fdeb6df60f10
ErickBGomez <erickbgomez.dev@gmail.com>
2025-05-15 11:43:13 -0600

    Add animation to show and hide search bar input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,6 +4,7 @@ import { Search } from "lucide-react";
 import { useRef } from "react";
 import Input from "../Input/Input";
 import styles from "./SearchBar.module.scss";
+import { motion, AnimatePresence } from "motion/react";
 
 const SearchBar = ({ active, setActive }) => {
   const navigate = useNavigate();
@@ -38,25 +39,48 @@ const SearchBar = ({ active, setActive }) => {
 
   return (
     <div className={styles.search}>
-      {active ? (
-        <form className={styles.searchForm} onSubmit={handleSearch}>
-          <Input
-            className={styles.input}
-            ref={inputRef}
-            variant="border"
-            onBlur={handleUnfocus}
-            placeholder="Find a item..."
-          />
-          {/* TODO: This button is not submitting the form */}
-          <button className={styles.activeSearchButton} type="submit">
+      {/* Separate icon button with form to activate animation correctly */}
+      <AnimatePresence mode="sync">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          key="search-button"
+        >
+          <IconButton
+            className={`${styles.searchButton} ${active ? styles.active : ""}`}
+            variant="ghost"
+            size="md"
+            onClick={handleClickWhenInactive}
+          >
             <Search />
-          </button>
-        </form>
-      ) : (
-        <IconButton variant="ghost" size="md" onClick={handleClickWhenInactive}>
-          <Search />
-        </IconButton>
-      )}
+          </IconButton>
+        </motion.div>
+        {active && (
+          <motion.div
+            initial={{ width: "25%", opacity: 0 }}
+            animate={{ width: "100%", opacity: 1 }}
+            exit={{ width: "25%", opacity: 0 }}
+            transition={{ duration: 0.2, ease: [0, 0.55, 0.45, 1] }}
+            key="search-form"
+          >
+            <form className={styles.searchForm} onSubmit={handleSearch}>
+              <Input
+                className={styles.input}
+                ref={inputRef}
+                variant="border"
+                onBlur={handleUnfocus}
+                placeholder="Find a item..."
+              />
+              {/* TODO: This button is not submitting the form */}
+              <button className={styles.activeSearchButton} type="submit">
+                <Search />
+              </button>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };

c5ad0a0dbedda1ab0a4534be0229b8a2e8423931
ErickBGomez <erickbgomez.dev@gmail.com>
2025-05-15 12:10:58 -0600

    Add animation to cart size bubble

--- src/components/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/HeaderCartButton/HeaderCartButton.jsx
@@ -1,11 +1,28 @@
 import { ShoppingCart } from "lucide-react";
 import CartContext from "@/context/CartContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import styles from "./HeaderCartButton.module.scss";
 import { IconButton } from "@chakra-ui/react";
+import { useAnimate } from "motion/react";
 
 const HeaderCartButton = () => {
   const { state, dispatch } = useContext(CartContext);
+  const [sizeScope, animate] = useAnimate();
+
+  // TODO: Play this animation 3 times only when cart size is changed
+  useEffect(() => {
+    animate(
+      sizeScope.current,
+      {
+        boxShadow: [
+          "0px 0px 0px 0px #3c908940",
+          "0px 0px 0px 8px #3c908940",
+          "0px 0px 0px 10px #3c908900",
+        ],
+      },
+      { duration: 1, ease: "linear" }
+    );
+  }, [animate, sizeScope]);
 
   return (
     <IconButton
@@ -16,7 +33,9 @@ const HeaderCartButton = () => {
     >
       <ShoppingCart />
       {state?.products?.length > 0 && (
-        <span className={styles.size}>{state.products.length}</span>
+        <span ref={sizeScope} className={styles.size}>
+          {state.products.length}
+        </span>
       )}
     </IconButton>
   );
